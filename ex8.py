c2a4c32e6939f4315d16a86d23dfe6dd66f5405c
adam <adam.fabricio@gmail.com>
2021-01-12 19:46:12 -0300

    Exercício 8

---
+++ ex8.py
@@ -0,0 +1,20 @@
+"""Exercício 8 - Printing, Printing."""
+
+
+formatter = "{} {} {} {}"  # Define uma variavel chamada formatter
+
+# Usa a função format da string formatter e passa a lista de parametros.
+print(formatter.format(1, 2, 3, 4))
+# Usa a função format da string formatter e passa a lista de parametros.
+print(formatter.format("one", "two", "three", "four"))
+# Usa a função format da string formatter e passa a lista de parametros.
+print(formatter.format(True, False, False, True))
+# Usa a função format da string formatter e passa a lista de parametros.
+print(formatter.format(formatter, formatter, formatter, formatter))
+# Usa a função format da string formatter e passa a lista de parametros.
+print(formatter.format(
+    "Try your",
+    "Own text here",
+    "Maybe a poem",
+    "Or a song about fear"
+))
