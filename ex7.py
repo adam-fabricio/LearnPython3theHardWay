9f66e37958668e79297a2bceb13d983ba7358b90
adam <adam.fabricio@gmail.com>
2021-01-12 19:08:38 -0300

    exercício 7 completo

---
+++ ex7.py
@@ -0,0 +1,43 @@
+"""Exercício 7 - More printing."""
+
+
+#  Escreve texto na linha.
+print("Mary have a little lamb.")
+#  Escreve texto na linha, escreve snow no lugar {}.
+print("It's fleece was wurte as {}.".format('snow'))
+#  Escreve outro texto na linha.
+print("And everywhere that Mary went.")
+#  Escreve 10 pontos.
+print("." * 10)  # what's that do?
+
+end1 = "C"  # Armazena caracter na variavel.
+end2 = "h"  # Armazena caracter na variavel.
+end3 = "e"  # Armazena caracter na variavel.
+end4 = "e"  # Armazena caracter na variavel.
+end5 = "s"  # Armazena caracter na variavel.
+end6 = "e"  # Armazena caracter na variavel.
+end7 = "B"  # Armazena caracter na variavel.
+end8 = "u"  # Armazena caracter na variavel.
+end9 = "r"  # Armazena caracter na variavel.
+end10 = "g"  # Armazena caracter na variavel.
+end11 = "e"  # Armazena caracter na variavel.
+end12 = "r"  # Armazena caracter na variavel.
+
+# watch end = ' ' at the end. Try removing it to see what happens
+# Cooncatena as strings contina nas variaveis, o parâmentro end= é
+# o que a função print vai fazer no final do comando.
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+print(end7 + end8 + end9 + end10 + end11 + end12)
+
+"""
+Study Drill.
+1 - Go back through and write a comment on what each line does.
+2 - Read each one backward or out loud to find your error.
+3 - From now on, when you make mistakes, write down on a piece of paper what
+    kind of mistake you made.
+4 - When you go to the next exercice, look at the mistake you have made and try
+    not to make then in this new one.
+5 - Remember that everyone make mistakes. Programmer are like magicians who
+    fool everyone into thinking they are perfect and never wrong. but it's all
+    an act. they make mistakes all the time.
+"""
