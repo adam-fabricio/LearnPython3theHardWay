dd6fd0033d9df6aabac4256c8f73fdfd345e33ff
adam <adam.fabricio@gmail.com>
2021-01-12 19:46:32 -0300

    Exercício 9

---
+++ ex9.py
@@ -0,0 +1,21 @@
+"""Exercício 9 - Printing, Printing, Printing."""
+
+
+# Here's sime new strange stuff, remeber type it exactly
+# Define uma strimg com nome dos dias da semana
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# Define uma string com os nomes dos meses com o caracterer especial \n
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# escreve na tela e concatena as variaveis
+print("Here are days: ", days)
+# escreve na tela e concatena as variaveis
+print("Here are the months: ", months)
+
+# Escreve um texto na tela.
+print("""
+There'ssomething going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6
+""")
