03e22a8f8b3fa8b94f5cb5cc98011682ec079687
adam-fabricio <adam.fabricio@gmail.com>
2021-01-21 13:05:51 -0300

    arrumado o erro de digitação

--- ex10.py
+++ ex10.py
@@ -8,5 +8,5 @@ height = input()
 print("How much do you weight?", end=' ')
 weight = input()
 
-print(f"So, you're {age} old, {height} tall and {weight} heavy."}
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
 
