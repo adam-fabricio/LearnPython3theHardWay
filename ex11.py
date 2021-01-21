e19c20540361ffaad716ee009e98324f838daa15
adam-fabricio <adam.fabricio@gmail.com>
2021-01-21 13:26:50 -0300

    Adicionado modificação para ver se funciona

--- ex11.py
+++ ex11.py
@@ -9,4 +9,7 @@ print("How much do you weight?", end=' ')
 weight = input()
 
 print(f"So, you're {age} old, {height} tall and {weight} heavy.")
-print('\n\n',
+
+print('\n\n', '*' * 60, '\n\n')
+
+print('Então, você tem {} anos, {} de altura e {} de peso.'.format(age, height, weigt))
