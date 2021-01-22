9d496864c04d8ab47dee46ce899a479036284189
Adam <adam-fabricio@gmail.com>
2021-01-22 17:56:55 -0300

    Exercicio 12 e renomeado os arquivos do exercício

---
+++ ex12.py
@@ -0,0 +1,9 @@
+""""Exercício 12 - Prompting People.~"""
+
+
+age = input("How old are you? ")
+height = input ("How tall are you? ")
+weight = input ("How much do you weight? ")
+
+print(f"So, you're {} old, {height} tall and {weight} heavy.")
+
