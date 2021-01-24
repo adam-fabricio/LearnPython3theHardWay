4f0a8e8b230819deb2d7f177368b261f9ac293c4
adam-fabricio <adam.fabricio@gmail.com>
2021-01-24 12:49:08 -0300

    ex16 study drill 2

--- ex16.py
+++ ex16.py
@@ -35,3 +35,19 @@ target.write('\n')
 print("And finally, we close it.")
 target.close()
 
+
+"""Study Drills
+1. Não aplicavel
+2. Write a script similar to the last exercise that uses read and argv to read the file ou just created
+3. There's too much repetitions in this file. Use strigs, formats, and escapes to print out line 1, line2 and line 3 with just one target.write() command insted of six)
+"""
+
+
+print('\n', 'x' * 20, ' Study drill - 2 ', 'x' * 20, sep='')
+
+txt = open(filename)
+
+print(f"Here´s is your file created: {filename}")
+print(txt.read())
+
+
