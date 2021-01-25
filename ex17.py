bc8a3b7530f74ecc8b6599e77b9351650e261dda
Adam <adam-fabricio@gmail.com>
2021-01-25 17:44:37 -0300

    Study Drills

--- ex17.py
+++ ex17.py
@@ -24,3 +24,40 @@ print("Alright, all done.")
 out_file.close()
 in_file.close()
 
+"""Study Drills
+1. The script is really annoying. There's no need to ask
+you before doing the copy, and it prints too much out 
+to the screen. Try to make the script  more friendly to 
+use by removing features.
+
+2. See now short you can make the script. I could make
+this one line long.
+
+3.
+"""
+
+print('\n', 'x' * 20, ' Study drill - 1 ', 'x' * 20, '\n', sep='')
+
+print(f"Deseja copiar o arquivo {from_file} para {to_file}?")
+print("Digite CTRL-C para cancelar, ou ENTER para continuar",
+      end='')
+input()
+
+arquivoDeEntrada = open(from_file)
+dadosDeEntrada = arquivoDeEntrada.read()
+
+arquivoDeSaida = open(to_file, 'w')
+arquivoDeSaida.write(dadosDeEntrada + "Study Drill 1")
+
+arquivoDeEntrada.close()
+arquivoDeSaida.close()
+
+print(f"Copiado {len(dadosDeEntrada)} bytes.")
+
+print('\n', 'x' * 20, ' Study drill - 2 ', 'x' * 20, '\n', sep='')
+
+input(f"Será copiado o arquivo {from_file} para {to_file}.")
+
+arquivoDeSaida = open(to_file, 'w')
+arquivoDeSaida.write(open(from_file).read() + "Study Drill 2")
+arquivoDeSaida.close()
