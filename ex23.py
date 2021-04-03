45eb1e7f3de0b9136edc2c358a81faa2f8a66324
adam <adam.fabricio@gmail.com>
2021-04-03 18:56:34 -0300

    ex23

--- ex23.py
+++ ex23.py
@@ -2,22 +2,27 @@
 
 
 import sys
-script, input_encoding, errors = sys.argv
+script, input_encoding, error = sys.argv
 
 def main(language_file, encoding, errors):
     line = language_file.readline()
     if line:
         print_line(line, encoding, errors)
-        return main(language_files, encoding, errors)
+        return main(language_file, encoding, errors)
 
 def print_line(line, encoding, errors):
     next_lang = line.strip()
     raw_bytes = next_lang.encode(encoding, errors=errors)
     cooked_string = raw_bytes.decode(encoding,errors=errors)
 
-    print(raw_bytes, "<===>", "utf-8")
+    print(raw_bytes, "<===>", cooked_string)
 
 languages = open("languages.txt", encoding="utf-8")
 
 main(languages, input_encoding, error)
+
+breaking_it3 = b'Rewrite this using the b\'\' bytes instead of the UTF-8 strings effectievely reversing the script'
+teste = "'''   . , "
+print(teste.encode())
+print(breaking_it3.decode().encode())
 
