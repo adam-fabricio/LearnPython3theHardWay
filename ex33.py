42a1dc08374b9ee39b6dd335c5df81a35e1ef5fc
adam <adam.fabricio@gmail.com>
2021-04-16 04:09:21 -0300

    Adicionado study drill 1

--- ex33.py
+++ ex33.py
@@ -16,3 +16,29 @@ print("The numbers: ")
 for num in numbers:
     print(num)
 
+''' Study drill - 1 -> Convert this while-loop to a function
+that you can call, and replace 6 in the test (i < 6) with
+a variable'''
+
+def lista_de_numeros(tamanho):
+    """Gera um lista de numero de números."""
+    numbers = []
+    i = 0
+
+    while i < tamanho:
+        print(f"At the top is {i}")
+        numbers.append(i)
+
+        i = i + 1
+        print("Numbers now: ", numbers)
+        print(f"At the bottom i is {i}")
+    
+    return numbers
+
+print("The numbers: ")
+
+numbers = lista_de_numeros(10)
+
+for num in numbers:
+    print(num)
+
