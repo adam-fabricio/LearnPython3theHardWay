ee94fa28fe290b0ad6ecfbe2afd85049d9f5b11c
adam <adam.fabricio@gmail.com>
2021-04-16 04:17:06 -0300

    ex33 study drills 3

--- ex33.py
+++ ex33.py
@@ -18,18 +18,24 @@ for num in numbers:
 
 ''' Study drill - 1 -> Convert this while-loop to a function
 that you can call, and replace 6 in the test (i < 6) with
-a variable'''
+a variable
+3 - Add another variable to the function arguments that 
+you can pass in that lets you change how much it 
+increments by.
+'''
 
-def lista_de_numeros(tamanho):
-    """Gera um lista de numero de números."""
+def lista_de_numeros(tamanho, incremento):
+    """Gera um lista de numero de números até o valor tamanho
+    de incremento em incremento."""
     numbers = []
     i = 0
+    
 
     while i < tamanho:
         print(f"At the top is {i}")
         numbers.append(i)
 
-        i = i + 1
+        i = i + incremento
         print("Numbers now: ", numbers)
         print(f"At the bottom i is {i}")
     
@@ -37,7 +43,7 @@ def lista_de_numeros(tamanho):
 
 print("The numbers: ")
 
-numbers = lista_de_numeros(10)
+numbers = lista_de_numeros(10, 2)
 
 for num in numbers:
     print(num)
