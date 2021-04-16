25cad27b92b1487223ee7e59c0e07d1d2b11411e
adam <adam.fabricio@gmail.com>
2021-04-16 03:51:48 -0300

    exercício 32 e 33, sem o study drills do exercício 33.

---
+++ ex33.py
@@ -0,0 +1,18 @@
+"""Exercício 33 - While Loops."""
+
+i = 0
+numbers = []
+
+while i < 6:
+    print(f"At the top is {i}")
+    numbers.append(i)
+
+    i = i + 1
+    print("Numbers now: ", numbers)
+    print(f"At the bottom i is {i}")
+
+print("The numbers: ")
+
+for num in numbers:
+    print(num)
+
