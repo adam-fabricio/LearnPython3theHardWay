2b29c53d3b3877b00be607116a94d8789bbd80b6
adam <adam.fabricio@gmail.com>
2021-04-21 07:11:20 -0300

    Digitado o ex38

---
+++ ex38.py
@@ -0,0 +1,27 @@
+"""Exercise 38 - Doing Things to List."""
+
+
+ten_things = "Apples Oranges Crows Telephone Light Sugar"
+
+print("Wait there are not 10 things in that list. Letś fix that.")
+
+stuff = ten_things.split(' ')
+more_stuff = ['Day', "Night", "Song", "Frisbee",
+              "Corn", "Banana", "Girl", "Boy"]
+
+while len(stuff) !=10:
+    nex_one = more_stuff.pop()
+    print("Adding: ", next_one)
+    stuff.append(next_one)
+    print(f"There are {len(stuff)} items now.")
+
+print("There we go: ", stuff)
+
+print("Let's do some things with stuff.")
+
+print(stuff[1])
+print(stuff[-1])  # Whoa! fancy
+print(stuff.pop())
+print(' '.join(stuff)) # What? cool!
+print('#'.join(stuff[3:5])) # Super stellar!
+
