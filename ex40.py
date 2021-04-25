0477340941da8bbd05b41cde0d7bf37fa05a018b
adam <adam.fabricio@gmail.com>
2021-04-25 19:08:31 -0300

    exercicio 40

---
+++ ex40.py
@@ -0,0 +1,22 @@
+"""Exercise 40 - Modules, Casses and Objects"""
+
+
+class Song(object):
+
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print(line)
+
+happy_bday = Song(["Hapy birthday to you",
+                   "I don't get want to get sued",
+                   "So I'll stop right there"])
+
+bulls_on_parade = Song(["They rally around the family",
+                       "With pockets full of shells"])
+
+happy_bday.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
