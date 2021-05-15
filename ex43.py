401d331a6d1643997323e04434588e162967d397
adam <adam.fabricio@gmail.com>
2021-05-15 00:45:21 -0300

    Realizado correção de digitação no exercício!

--- ex43.py
+++ ex43.py
@@ -20,18 +20,18 @@ class Engine(object):
         self.scene_map = scene_map
 
     def play(self):
-        current_scene = self.scene_map.opening_scene('finished')
-        current_scene = self.scene_map.next_scene('finished')
+        current_scene = self.scene_map.opening_scene()
+        last_scene = self.scene_map.next_scene('finished')
 
         while current_scene != last_scene:
-            next_scene_name = currnet_scene.enter()
+            next_scene_name = current_scene.enter()
             current_scene = self.scene_map.next_scene(next_scene_name)
 
         # be sure to print the last scene
         current_scene.enter()
 
 
-class Deth(Scene):
+class Death(Scene):
 
     quips = [
         "You died.  You kinda suck at this.",
@@ -241,13 +241,13 @@ class Finished(Scene):
 
 class Map(object):
 
-    scene = {
-        'central_corridor': CentralCorridor():,
-        'laser_weapon_armory':LaserWeaponArmory(),
-        'the_bridge':TheBridge(),
-        'escape_pod':EscapePod(),
-        'death':Death(),
-        'finished':Finished()
+    scenes = {
+        'central_corridor': CentralCorridor(),
+        'laser_weapon_armory': LaserWeaponArmory(),
+        'the_bridge': TheBridge(),
+        'escape_pod': EscapePod(),
+        'death': Death(),
+        'finished': Finished()
     }
     def __init__(self, start_scene):
         self.start_scene = start_scene
