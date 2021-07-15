c3c3ae7940244b22ea606afdae1c9078963b069e
Adam <adam@adam.com>
2021-07-15 20:45:57 -0300

    Feature: Exercício 49 incompleto.

---
+++ projects/ex48/ex48/parser.py
@@ -0,0 +1,11 @@
+"""Exercise 49 - Making Sentences."""
+
+
+class ParserError(Exception):
+    """Create a error with the sentence does not make a sence."""
+    pass
+
+
+def parse_sentence(sentence):
+    """Return a object with a three atribute."""
+    for 
---
+++ projects/ex48/tests/parser_tests.py
@@ -0,0 +1,19 @@
+"""Exercise 49 test."""
+
+
+from nose.tools import *
+from ex48 import lexicon, parser
+
+
+def test_parser():
+    sentence_list = lexicon.scan("run north")
+    sentence = parse_sentence(sentence_list)
+    result = [sentence.subject, sentence.verb, sentence.object]
+    assert_equal(result, ['player', 'run', 'north']
+
+    sentence_list = lexicon.scan("bear eat the honey")
+    sentence = parse_sentence(sentence_list)
+    result = [sentence.subject, sentence.verb, sentence.object]
+    assert_equal(result, ['bear', 'eat', 'honey'])
+
+
